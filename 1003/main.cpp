9840c4031e24468cd744761fe2d8c7d55ebc8ee4
ruyadisk <84979672+ruyadisk@users.noreply.github.com>
2023-10-02 18:25:30 +0000

    updated

--- 1003/main.cpp
+++ 1003/main.cpp
@@ -1,137 +1,170 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <string>
 #include <stack>
 #include <vector>
 #include <math.h>
+#include <iomanip>
 
+using namespace std;
 
-double performOperation(double num1, double num2, char op) {
-    switch (op) {
-        case '+':
-            return num1 + num2;
-        case '-':
-            return num1 - num2;
-        case '*':
-            return num1 * num2;
-        case '/':
-            if (num2 != 0.0) {
-                return num1 / num2;
-            } else {
-                std::cerr << "Error: Division by zero!" << std::endl;
-                exit(1);
-            }
-        case '^':
-            return std::pow(num1, num2);
-        default:
-            std::cerr << "Error: Invalid operator '" << op << "'" << std::endl;
-            exit(1);
+string digits = "0123456789";
+
+bool priority(char top, char now) {
+    if (top == '(') {
+        return false;
+    } else if ((top == '-' || top == '+' || top == '^') && (now == '*' || now == '/')) {
+        return false;
     }
+    return true;
 }
 
-int priority(char c){
-    if(c == '+' || c == '-'){
-        return 1;
-    }
-    if(c == '*' || c == '/'){
-        return 2;
-    }
-    if(c == '^'){
-        return 3;
-    }
-    return -1;
+void perform(stack<double> &number_stack, stack<char> &operator_stack) {
+    double second = number_stack.top();
+    number_stack.pop();
+    double first = number_stack.top();
+    number_stack.pop();
+    char op = operator_stack.top();
+    operator_stack.pop();
+    double result;
+    if (op == '+') result = first + second;
+    else if (op == '-') result = first - second;
+    else if (op == '*') result = first * second;
+    else if (op == '/') result = first / second;
+    else if (op == '^') result = pow(first, second);
+    number_stack.push(result);
+    return;
 }
 
-bool isdigit(char c){
-    return (c >='0' && c <= '9');
-}
-
-double evaluateExpression(const std::string &expression) {
-    std::cout << expression << std::endl;
-    std::istringstream iss(expression);
-    std::stack<double> numStack;
-    std::stack<char> opStack;
-    char ch;
-
-    while (iss >> ch) {
-        if (isdigit(ch) || ch == '.') {
-            iss.putback(ch);
-            double num;
-            iss >> num;
-            numStack.push(num);
-        } else if (ch == '(') {
-            opStack.push(ch);
-        } else if (ch == ')') {
-            while (!opStack.empty() && opStack.top() != '(') {
-                char op = opStack.top();
-                opStack.pop();
-                double num2 = numStack.top();
-                numStack.pop();
-                double num1 = numStack.top();
-                numStack.pop();
-                numStack.push(performOperation(num1, num2, op));
+double analyze(const string &exp) {
+    stack<double> number_stack;
+    stack<char> operator_stack;
+    operator_stack.push('(');
+    string exp_ = exp + ')';
+    bool ifnext = false;
+    for (int i = 0; i < exp_.size(); ++i) {
+        if(exp_[i] == ' '){
+            continue;
+        }
+        else if (exp_[i] == '(' || exp_[i] == '[' || exp_[i] == '{') {
+            operator_stack.push('(');
+        } else if (exp_[i] == ')' || exp_[i] == ']' || exp_[i] == '}') {
+            while (operator_stack.top() != '(')
+                perform(number_stack, operator_stack);
+            operator_stack.pop();
+        } else if (ifnext) {
+            while (priority(operator_stack.top(), exp_[i])) {
+                perform(number_stack, operator_stack);
             }
-            if (!opStack.empty() && opStack.top() == '(') {
-                opStack.pop(); // 弹出左括号
-            } else {
-                std::cerr << "Error: Unmatched ')' in expression." << std::endl;
-                exit(1);
-            }
-        } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^') {
-            while (!opStack.empty() && priority(ch) <= priority(opStack.top())) {
-                char op = opStack.top();
-                opStack.pop();
-                double num2 = numStack.top();
-                numStack.pop();
-                double num1 = numStack.top();
-                numStack.pop();
-                numStack.push(performOperation(num1, num2, op));
-            }
-            opStack.push(ch);
+            operator_stack.push(exp_[i]);
+            ifnext = false;
+        } else {
+            int j = i;
+            if (exp_[j] == '+' || exp_[j] == '-' || exp_[j] == '*' || exp_[j] == '/' || exp_[j] == '^') ++i;
+            while (digits.find(exp_[i]) != digits.npos) ++i;
+            number_stack.push((double)stoi(exp_.substr(j, i - j)));
+            --i;
+            ifnext = true;
         }
     }
-    while (!opStack.empty()) {
-        char op = opStack.top();
-        opStack.pop();
-        double num2 = numStack.top();
-        numStack.pop();
-        double num1 = numStack.top();
-        numStack.pop();
-        numStack.push(performOperation(num1, num2, op));
-    }
-    return numStack.top();
+    return number_stack.top();
 }
 
-std::vector<double> vec_calculate(std::vector<std::vector<float>>table, std::string vec_postfix){
-    
+vector<double> vec_calculate(vector<vector<double>> table, string vec_postfix){
+    int pos = 0;
+    stack<int> s;
+    for(int cnt = 0; cnt < vec_postfix.size(); cnt++){
+        int tmp = 0;
+        if(digits.find(vec_postfix[cnt]) != digits.npos){
+            pos = cnt;
+            while(digits.find(vec_postfix[cnt]) != digits.npos){
+                ++cnt;
+            }
+            s.push(stod(vec_postfix.substr(pos, cnt - pos)));
+            --cnt;
+        }
+        else if(vec_postfix[cnt] == '+' || vec_postfix[cnt] == '-' || vec_postfix[cnt] == '*' || vec_postfix[cnt] == '/' || vec_postfix[cnt] == '^'){
+            vector<double>& first = table[s.top()];
+            s.pop();
+            vector<double>& second = table[s.top()];
+            int flg = s.top();
+            s.pop();
+            int size_first = first.size();
+            int size_second = second.size();
+            if(size_first > size_second){
+                int size_dif = size_first - size_second;
+                for(int cnt_ = 0; size_dif > 0; size_dif--){
+                    second.push_back(second[cnt_]);
+                    cnt_++;
+                }
+            }
+            else if(size_first < size_second){
+                int size_dif = size_second - size_first;
+                for(int cnt_ = 0; size_dif > 0; size_dif--){
+                    first.push_back(first[cnt_]);
+                    cnt_++;
+                }
+            }
+            switch(vec_postfix[cnt]){
+                case '+':
+                for(int vec_cnt = 0; vec_cnt < second.size(); vec_cnt++){
+                    second[vec_cnt] += first[vec_cnt];
+                }
+                break;
+                case '-':
+                for(int vec_cnt = 0; vec_cnt < second.size(); vec_cnt++){
+                    second[vec_cnt] -= first[vec_cnt];
+                }
+                break;
+                case '*':
+                for(int vec_cnt = 0; vec_cnt < second.size(); vec_cnt++){
+                    second[vec_cnt] *= first[vec_cnt];
+                }
+                break;
+                case '/':
+                for(int vec_cnt = 0; vec_cnt < second.size(); vec_cnt++){
+                    second[vec_cnt] /= first[vec_cnt];
+                }
+                break;
+                case '^':
+                for(int vec_cnt = 0; vec_cnt < second.size(); vec_cnt++){
+                    second[vec_cnt] = pow(second[vec_cnt],first[vec_cnt]);
+                }
+                break;
+            }
+            s.push(flg);
+        }
+        else{
+            continue;
+        }
+    }
+    return table[s.top()];
 }
 
 
 int main(int argc, char* argv[]){
-    std::ifstream fin;
+    ifstream fin;
     fin.open(argv[1]);
     
-    std::vector<std::string>line_table;
-    std::string buf;
-    while(std::getline(fin, buf)){    
+    vector<string>line_table;
+    string buf;
+    while(getline(fin, buf)){    
         line_table.push_back(buf);
     }
     
     for(auto &it : line_table){
-        std::vector<std::vector<double>>table;
-        std::stack<char> vec_proc;
-        std::string vec_postfix;
+        vector<vector<double>>table;
+        stack<char> vec_proc;
+        string vec_postfix;
         int vec_cnt = 0;
         for(int cnt = 0; cnt < it.size(); cnt++){
-            std::vector<double>element;
+            vector<double>element;
             if(it[cnt] == '['){
                 cnt++;
-                std::string res = "";
+                string res = "";
                 while(it[cnt] != ']'){
                     if(it[cnt] == ','){
-                        std::cout << res << std::endl;
-                        double tmp = evaluateExpression(res);
+                        double tmp = analyze(res);
                         element.push_back(tmp);
                         res = "";
                         cnt++;
@@ -141,13 +174,11 @@ int main(int argc, char* argv[]){
                     }
                     else{
                         res += it[cnt];
-                        res += " ";
-                        std::cout << res << std::endl;
                         cnt++;
                     }
                 }
-                element.push_back(evaluateExpression(res));
-                vec_postfix += std::to_string(vec_cnt);
+                element.push_back(analyze(res));
+                vec_postfix += to_string(vec_cnt);
                 vec_postfix += " ";
                 table.push_back(element);
                 vec_postfix += vec_cnt;
@@ -157,8 +188,8 @@ int main(int argc, char* argv[]){
             else if(it[cnt] == ' ' || (int)it[cnt] == 13){
                 continue;
             }
-            else{ // +,-,*,/ ...
-                while(!vec_proc.empty() && (priority(it[cnt])) <= priority(vec_proc.top())){
+            else{
+                while(!vec_proc.empty() && priority(vec_proc.top(), it[cnt])){
                     vec_postfix += vec_proc.top();
                     vec_postfix += " ";
                     vec_proc.pop();
@@ -170,14 +201,10 @@ int main(int argc, char* argv[]){
             vec_postfix += vec_proc.top();
             vec_proc.pop();
         }
-        for(auto& tblit : table){
-            for(auto& tblit_ : tblit){
-                std::cout << tblit_ << " ";
-            }
-            std::cout << std::endl;
+        for(auto &it : vec_calculate(table, vec_postfix)){
+            cout << fixed << setprecision(2) << it << " ";
         }
-        std::cout << vec_postfix << std::endl;
-        std::cout << calculate
+        cout << endl;
     
     }
 }
